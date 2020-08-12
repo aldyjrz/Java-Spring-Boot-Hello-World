c29b8b59484e83b90226094ef45e852e16c12e64
Aldi Pranata <aldyjrz@gmail.com>
2020-08-13 01:48:13 +0700

    init commit

---
+++ src/main/java/com/example/toi/belajar/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.toi.belajar;
+
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	 
+	@RequestMapping("/")
+	public String hello() {
+		 
+		return "Hello, AldyToi";
+	}
+
+}
